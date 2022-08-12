a2b26605af24284a06d09de2966d94593ca1e2c3
Michaela Varberg <michaelavarberg@gmail.com>
2022-08-12 17:18:58 -0600

    time blocks background color loads based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-// var header = document.querySelector("#header");
-
-// $(header).on("click", function () {
-//   console.log("working!");
-// });
 var today = new Date();
 var weekdays = [
   "Sunday",
@@ -30,6 +25,22 @@ var months = [
 var day = weekdays[today.getDay()];
 var month = months[today.getMonth()];
 var date = today.getDate();
+var timeBlocksList = $("#time-blocks").children();
+var currentHour = today.getHours();
+
+console.log(timeBlocksList.eq(5).attr("id"));
+
+timeBlocksList.children().each(function (index) {
+  var time = timeBlocksList.eq(index).attr("id");
+  var block = timeBlocksList.eq(index);
+  if (time < currentHour) {
+    block.addClass("past");
+  } else if (time > currentHour) {
+    block.addClass("future");
+  } else {
+    block.addClass("present");
+  }
+});
 
 $("#currentDay").text(day + ", " + month + " " + date + nth(date));
 
@@ -46,3 +57,10 @@ function nth(d) {
       return "th";
   }
 }
+//make an array of timeblocks
+//Write code to change class of a time-block based on if it is before, during or after a time
+//Write code to take input from any text area, record its position, push it to an array of objects
+//Write code to save array to local storage
+//Write code to render events from local storage and print to screen on load: helpful
+// To write text to any of the time blocks
+// timeBlocksArray.children().eq(0).children().eq(1).text("Hello");
