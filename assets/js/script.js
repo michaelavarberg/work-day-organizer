b9b26570aee253c68e1155cb4a3598ecca311875
Michaela Varberg <michaelavarberg@gmail.com>
2022-08-12 18:19:17 -0600

    allows new items to be saved in local storage and displayed on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,12 +27,31 @@ var month = months[today.getMonth()];
 var date = today.getDate();
 var timeBlocksList = $("#time-blocks").children();
 var currentHour = today.getHours();
+var timeBlocksSection = $("#time-blocks");
+var savedEvents = JSON.parse(localStorage.getItem("saved-events"));
 
-console.log(timeBlocksList.eq(5).attr("id"));
-
+if (savedEvents === null) {
+  savedEvents = ["", "", "", "", "", "", "", "", ""];
+} else {
+  timeBlocksList.children().each(function (index) {
+    timeBlocksList
+      .children()
+      .eq(index)
+      .children()
+      .eq(1)
+      .text(savedEvents[index]);
+  });
+}
+//Loads any saved events
+timeBlocksList.children().each(function (index) {});
+//Saves array to local storage
+function storeSchedule() {
+  localStorage.setItem("saved-events", JSON.stringify(savedEvents));
+}
+//Loads background color of time blocks based on current time
 timeBlocksList.children().each(function (index) {
   var time = timeBlocksList.eq(index).attr("id");
-  var block = timeBlocksList.eq(index);
+  var block = timeBlocksList.eq(index).children().children().eq(1);
   if (time < currentHour) {
     block.addClass("past");
   } else if (time > currentHour) {
@@ -41,9 +60,12 @@ timeBlocksList.children().each(function (index) {
     block.addClass("present");
   }
 });
-
+//Loads saved text into correct blocks
+function init() {}
+//Loads current date in the header
 $("#currentDay").text(day + ", " + month + " " + date + nth(date));
 
+//Adds correct ending to date in the header
 function nth(d) {
   if (d > 3 && d < 21) return "th";
   switch (d % 10) {
@@ -57,10 +79,20 @@ function nth(d) {
       return "th";
   }
 }
-//make an array of timeblocks
-//Write code to change class of a time-block based on if it is before, during or after a time
+
+timeBlocksSection.on("click", ".fas", function (event) {
+  var index = $(event.target).parent().parent().parent().attr("id");
+  index = parseInt(index);
+  index -= 9;
+  var item = $(event.target).parent().prev().val();
+  savedEvents.splice(index, 1, item);
+  storeSchedule();
+});
+
+// console.log(JSON.parse("saved-events"));
 //Write code to take input from any text area, record its position, push it to an array of objects
 //Write code to save array to local storage
 //Write code to render events from local storage and print to screen on load: helpful
 // To write text to any of the time blocks
 // timeBlocksArray.children().eq(0).children().eq(1).text("Hello");
+console.log(savedEvents);
